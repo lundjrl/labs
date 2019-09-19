b0b98dd416012eda8ee881f869720f8c27d282b8
lundjrl <jrlbidamin@gmail.com>
2019-09-18 20:38:22 -0400

    Fork bomb issue..

--- lab3/prog.c
+++ lab3/prog.c
@@ -1,58 +1,64 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <sys/time.h>
+#include <time.h>
 
 void handler(int sig){
   if (sig == SIGUSR1){
-    printf(" Caught signal %d\n", sig);
-    kill(0, sig);
+    printf(" Caught signal SIGUSR1\n");
   }
   else if (sig == SIGUSR2){
-    printf(" Caught signal %d\n", sig);
-    kill(0, sig);
+    printf(" Caught signal SIGUSR2\n");
   }
   else if (sig == SIGINT){
-    printf(" Caught signal %d\n", sig);
-    kill(0, sig);
+    printf(" Shutting down...\n");
+    exit(0);
   }
-  printf("handler");
+  else{
+    printf(stderr, "Signal error\n");
+  }
 }
 
-int main(){
 
-  pid_t pid;
+int main(){
+  pid_t parent, pid;
+  int status, sleptfor;
   time_t t;
   int fd[2];
 
-  signal(SIGUSR1, handler);
-  signal(SIGUSR2, handler);
-  signal(SIGINT, handler);
+  while(1){
 
-  if (pipe (fd) < 0) { 
-        perror ("plumbing problem"); 
-        exit(1); 
-  } 
+	  parent = getpid();
+	  signal(SIGUSR1, handler);
+	  signal(SIGUSR2, handler);
+	  signal(SIGINT, handler);
 
-  if ((pid =  fork()) < 0){
-    perror("fork failed");
-    exit(1);
-  }
+	  if ((pid =  fork()) < 0){
+	    perror("fork failed");
+	    exit(1);
+	  }
 
-  else if (pid == 0){
-    //Child process
-    srand((unsigned) time(&t));
-    wait(rand() % 5);
-    printf("child");
-    //kill(pid, SIGUSR2);
-  }
-  else{
-  //Parent process
-  signal(SIGUSR1, handler);
-  signal(SIGUSR2, handler);
-  sleep(10);
+	  else if (pid == 0){
+	    //Child process
+	    srand((unsigned) time(&t));
+	    sleptfor = (rand() % 5);
+	    if (sleptfor % 2 == 0){
+	      sleep(sleptfor);
+	      kill(parent, SIGUSR1);
+	    }
+	    else{
+	      kill(parent, SIGUSR2);
+	      exit(0);
+	    }
+	  }
+	  else{
+	  //Parent process
+	  printf("Waiting on child...");
+	  wait(&status);
+	  }
   }
   return 0;
 }
