1aa9b957256918b07f70794628c1491849b6ddaa
lundjrl <jrlbidamin@gmail.com>
2019-09-23 14:25:53 -0400

    fileserver update

--- lab4/fileserver.c
+++ lab4/fileserver.c
@@ -12,11 +12,22 @@ int main(int argc, char* argv[]){
 	fgets(input, 50, stdin);
 	
 	pthread_t thread1;
-
-	if ((status = pthread_create (&thread1, NULL, somefunc, info passed)) != 0 ){
-		fprintf (stderr, "failed to create thread %d: %s\n", status strerror(status));
-		exit(1);
+	// while(1){
+		if ((status = pthread_create (&thread1, NULL, somefunc, input)) != 0 ){
+			fprintf (stderr, "Failed to create thread %d: %s\n", status strerror(status));
+			exit(1);
+		}
 	}
 
+}
 
+int processfile(int stufffromdispatcher){
+	pthread_t wt1;
+	int status;
+	printf("Processing file from thread %d\n", stufffromdispatcher);
+	//do stuff for creating worker threads
+	if ((status = pthread_create(&wt1, NULL, function to do, stufffromdispatcher)) != 0){
+		fprintf(stderr, "Failed to create worker. Error %d: %s\n", status, stderror(status));
+		exit(1);
+	return 0;
 }
