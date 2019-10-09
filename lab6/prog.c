1bc90ba2790a645d818df102bb41189fcd6959b7
lundjrl <jrlbidamin@gmail.com>
2019-10-09 18:51:11 -0400

    Started lab6 practical

--- lab6/prog.c
+++ lab6/prog.c
@@ -6,6 +6,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <semaphore.h>
 
 #define SIZE 16
 //FREE YOUR RESOURCES, MAKE SURE EVERY LINE OF CODE IS USED
@@ -15,9 +16,11 @@ int main (int argc, char* argv[])
    long int i, loop, temp, *shmPtr;
    int shmId;
    pid_t pid;
+   sem_t mutex;
    loop = atoi(argv[1]);
       // get value of loop variable (from command-line argument)
 
+   sem_init(&mutex, 0, 1);
    if ((shmId = shmget (IPC_PRIVATE, SIZE, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
       perror ("i can't get no..\n");
       exit (1);
@@ -31,12 +34,14 @@ int main (int argc, char* argv[])
    shmPtr[1] = 1;
 
    if (!(pid = fork())) {
+      wait(&mutex);
       for (i=0; i<loop; i++) {
                // swap the contents of shmPtr[0] and shmPtr[1]
         temp = shmPtr[0];
         shmPtr[0] = shmPtr[1];
         shmPtr[1] = temp;
       }
+      signal(&mutex);
       if (shmdt (shmPtr) < 0) {
          perror ("just can't let go\n");
          exit (1);
@@ -44,6 +49,7 @@ int main (int argc, char* argv[])
       exit(0);
    }
    else
+      wait(&mutex);
       for (i=0; i<loop; i++) {
                // swap the contents of shmPtr[1] and shmPtr[0]
         temp = shmPtr[1];
@@ -51,6 +57,7 @@ int main (int argc, char* argv[])
         shmPtr[0] = temp;
 
       }
+      signal(&mutex);
 
    wait (&status);
    printf ("values: %li\t%li\n", shmPtr[0], shmPtr[1]);
@@ -63,6 +70,8 @@ int main (int argc, char* argv[])
       perror ("can't deallocate\n");
       exit(1);
    }
+
+   sem_destroy(&mutex);
 
    return 0;
 }
