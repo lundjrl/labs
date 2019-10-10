7ffdf697d034a1d0822265979aa4525e6438f618
zachary-thomas <thomazac@mail.gvsu.edu>
2019-10-10 13:39:53 -0400

    fix errors

--- lab7/prog1.c
+++ lab7/prog1.c
@@ -1,7 +1,9 @@
 #include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  printf("%d",sysconf(SEM_NSEMS_MAX))
+  printf("%d",sysconf(SEM_NSEMS_MAX));
 
   exit(0);
 }
