c69e0126aa67309add3414e0a3fa3eaaa8773f81
zachary-thomas <thomazac@mail.gvsu.edu>
2019-10-30 17:50:27 -0400

    fixed prog1.c for lab9

--- lab9/prog1.c
+++ lab9/prog1.c
@@ -9,7 +9,7 @@ int main()
   char *data1;
 
   data1 = malloc (SIZE);
-  printf ("Please input username: ");
+  printf ("Please input username (Only 15 characters will be saved): ");
   fgets(data1, SIZE, stdin);
   printf ("you entered: %s\n", data1);
   free (data1);
