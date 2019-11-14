a8ba51d32d7d48cedcf38c1484831da56ef3826a
lundjrl <jrlbidamin@gmail.com>
2019-11-14 18:19:39 -0500

    tried our best`

--- lab12/ls.c
+++ lab12/ls.c
@@ -5,29 +5,86 @@
 #include <errno.h>
 #include <dirent.h>
 #include <string.h>
+#include <time.h>
 
 int main(int argc, char *argv[]){
 
     DIR *dirPtr;
+    int multiArgs = 1;
+    long totalBlocks = 0;
     struct dirent *entryPtr;
     struct stat statBuf;
-    if(argv[2] == NULL){
-        dirPtr = opendir(".");
+    struct tm timeStruct;
+    char buf[80];
+
+    // See how many arguments are passed and if it is an option or directory
+    if(argc == 1){
+      dirPtr = opendir(".");
+      multiArgs = 0;
     } else {
+      if(argv[1] != NULL && argv[1][0] != '-'){
+        dirPtr = opendir(argv[1]);
+      }
+
+
+      if(argv[2] == NULL){
+        dirPtr = opendir(".");
+      } else {
         dirPtr = opendir(argv[2]);
+      }
+
     }
 
     while((entryPtr = readdir (dirPtr))){
-        if(strcmp(argv[1], "-n")){
-//            printf("Permissions: %-10s\n 
+      if(multiArgs){
+        if(strcmp(argv[1], "-n") == 0){
+
+          if((strcmp(entryPtr->d_name, ".") == 0) || (strcmp(entryPtr->d_name, "..") == 0)){
+            continue;
+          }
+
+           stat(entryPtr->d_name, &statBuf);
+           printf("%ld\n", statBuf.st_blksize);
+           printf((S_ISDIR(statBuf.st_mode)) ? "d" : "-");
+           printf((statBuf.st_mode & S_IRUSR) ? "r" : "-");
+           printf((statBuf.st_mode & S_IWUSR) ? "w" : "-");
+           printf((statBuf.st_mode & S_IXUSR) ? "x" : "-");
+           printf((statBuf.st_mode & S_IRGRP) ? "r" : "-");
+           printf((statBuf.st_mode & S_IWGRP) ? "w" : "-");
+           printf((statBuf.st_mode & S_IXGRP) ? "s" : "-");
+           printf((statBuf.st_mode & S_IROTH) ? "r" : "-");
+           printf((statBuf.st_mode & S_IWOTH) ? "w" : "-");
+           printf((statBuf.st_mode & S_IXOTH) ? "x" : "-");
+           printf("%ld\t", statBuf.st_nlink);
+           printf("%d\t", statBuf.st_uid);
+           printf("%d\t", statBuf.st_gid);
+
+           totalBlocks += statBuf.st_size;
+           printf("%ld\t", statBuf.st_size);
+
+           timeStruct = *localtime(&statBuf.st_mtim.tv_sec);
+           strftime(buf, sizeof(buf), "%m %d %H:%M", &timeStruct);
+           printf("%s\t", buf);
+
+           printf("%-10s\n", entryPtr->d_name);
         }
         if((strcmp(argv[1], "-i")) == 0){
-            printf ("inode: %-10lu  name: %-10s\n",entryPtr->d_ino, entryPtr->d_name);
+          printf ("inode: %-10lu  name: %-10s\n",entryPtr->d_ino, entryPtr->d_name);
         }
-        else{
+      }
+      else{
         printf("name: %-20s\n", entryPtr->d_name);
-        }
-    }   
+      }
+    }
 
+    if(multiArgs){
+      if(strcmp(argv[1], "-n") == 0){
+        printf("Total: %ld\n", (totalBlocks/1024));
+      }
+    }
+
+
+    exit(1);
 
 }
+
