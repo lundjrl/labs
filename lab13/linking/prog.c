2a09ebeac6794835ca64220f3486719c1bc4ebad
lundjrl <jrlbidamin@gmail.com>
2019-12-03 07:41:02 -0500

    Finished lab

--- lab13/linking/prog.c
+++ lab13/linking/prog.c
@@ -5,21 +5,22 @@
 
 int main(int argc, char *argv[]){
 
-  if(argc != 3){
+  if(argc < 3){
      printf("Please use format: %s <src_file> <dest_file>\n",argv[0]);
      exit(0);
   }
-  if(link(argv[1], argv[2]) == -1){
-     printf("Link Error\n");
-     exit(1);
-  }
-  if((strcmp(argv[2], "-s")) == 0){
-    if(symlink(argv[3], argv[4]) == -1){
-       printf("Link Error\n");
+  if((strcmp(argv[1], "-s")) == 0){
+    if(symlink(argv[2], argv[3]) == -1){
+       printf("Link s Error\n");
        exit(1);
     }
     else{
        printf("Symlink created\n");
+    }
+  }else{
+    if(link(argv[1], argv[2]) == -1){
+       printf("Link Error\n");
+       exit(1);
     }
   }
 
